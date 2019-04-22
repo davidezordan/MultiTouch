6f48f45deed3b2ad31578def890d76cdcfa7991d
Davide Zordan <info@davidezordan.net>
2019-04-22 19:46:05 +0100

    Added new Visual Studio 2019 solution

---
+++ Xamarin.Forms/MultiTouch/MultiTouch/Helpers/Infrastructure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MultiTouch.Behaviors.Helpers
+{
+    /// <summary>
+    /// Infrastructure helper class needed for initialising behaviors when deploying different assemplies to iOS devices
+    /// </summary>
+    public static class Infrastructure
+	{
+		public static void Init()
+		{
+		}
+	}
+}
+
